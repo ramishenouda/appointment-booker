3ccf49fba4db9800bd149f21c72dda1979238e42
ramishenouda <ramishenouda@outlook.com>
2020-09-23 14:18:10 +0200

    Added form validation

--- appointment-booker/src/components/appointment/appointment-container.js
+++ appointment-booker/src/components/appointment/appointment-container.js
@@ -1,11 +1,179 @@
 import React, { Component } from 'react';
 
+import CountryCodes from './country-codes'
+
 import AppointmentView from './appointment-view'
 
 class Appointment extends Component {
+    state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        countryCode: '+20',
+        phoneNumber: '',
+        operationCountries: '',
+        companyName: '',
+        objective: '',
+        description: '',
+        openedMenu: ''
+    }
+
+    requestAppointment = (event) => {
+        event.preventDefault();
+        if (this.validateForm()) {
+            console.log('booking');
+        } else {
+            console.log('error');
+        }
+    }
+
+    // todo find a better way in react to check for form validation
+    validateForm = () => {
+        const re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+        const emailValidation = new RegExp(re).test(this.state.email);
+    
+        let validForm = true;
+    
+        // todo find an API to check for phoneNumbers
+        if (this.state.phoneNumber.length === 10 || this.state.phoneNumber.length === 11) {
+            this.setState({ validPhoneNumber: true });
+        } else {
+            this.setState({ validPhoneNumber: false });
+            validForm = false;
+        }
+    
+        if (emailValidation) {
+            this.setState({ validEmail: true });
+        } else if (this.state.email.length > 0) {
+            this.setState({ validEmail: false });
+            validForm = false;
+        } else {
+            this.setState({ validEmail: false });
+            validForm = false;
+        }
+
+        if (this.state.firstName.length > 1) {
+            this.setState({ validFirstName: true })
+        } else {
+            this.setState({ validFirstName: false })
+            validForm = false
+        }
+
+        if (this.state.lastName.length > 1) {
+            this.setState({ validLastName: true })
+        } else {
+            this.setState({ validLastName: false })
+            validForm = false
+        }
+
+        if (this.state.operationCountries.length > 0) {
+            this.setState({ validOperationCountries: true })
+        } else {
+            this.setState({ validOperationCountries: false })
+            validForm = false
+        }
+
+        if (this.state.companyName.length > 0) {
+            this.setState({ validCompanyName: true })
+        } else {
+            this.setState({ validCompanyName: false })
+            validForm = false
+        }
+
+        if (this.state.objective.length > 0) {
+            this.setState({ validObjective: true })
+        } else {
+            this.setState({ validObjective: false })
+            validForm = false
+        }
+
+        if (this.state.description.length > 0) {
+            this.setState({ validDescription: true })
+        } else {
+            this.setState({ validDescription: false })
+            validForm = false
+        }
+    
+        return validForm;
+    }
+
+    setOpenedMenu = (name) => {
+        this.setState({ openedMenu: name })
+    }
+
+    handleChange = (event) => {
+        let name, value = '';
+
+        if (event === null) {
+            this.setState({ [this.state.openedMenu]: '' });
+            return;
+        }
+
+        if (!event.target) {
+            if(event[0]) {
+                if (event[0].name)
+                    name = event[0].name;
+                else
+                    name = this.state.openedMenu;
+
+                event.forEach(element => {
+                    if (value !== '')
+                        value += ', '
+                    value += element.value;
+                });
+            } else {
+                if (event.name)
+                    name = event.name;
+                else 
+                    name = this.state.openedMenu;
+                value = event.value;
+            }
+        } else {
+            name = event.target.name;
+            value = event.target.value
+        }
+
+        this.setState({[name]: value});
+    }
+    
     render() {
+        
+        const countryOptions = [
+            { value: 'country1', name: 'operationCountries', label: 'Country1' },
+            { value: 'country2', name: 'operationCountries', label: 'Country2' },
+            { value: 'country3', name: 'operationCountries', label: 'Country3' }
+        ]
+
+        const companyOptions = [
+            { value: 'company1', name: 'companyName', label: 'Company1' },
+            { value: 'company2', name: 'companyName', label: 'Company2' },
+            { value: 'company3', name: 'companyName', label: 'Company3' }
+        ]
+
+        const objectiveOptions = [
+            { value: 'complaint', name: 'objective', label: 'Complaint' },
+            { value: 'suggestion', name: 'objective', label: 'Suggestion' }
+        ]
+
+        const countryCodesOptions = CountryCodes.map((code, index) => {
+            return (
+                <option key={index} value={code.dial_code}>
+                    {`${code.code} ${code.dial_code}`} 
+                </option>
+            );
+        });
+        
         return (
-            <AppointmentView />
+            <AppointmentView
+                requestAppointment={this.requestAppointment} 
+                handleChange={this.handleChange}
+                setOpenedMenu={this.setOpenedMenu}
+                countryOptions={countryOptions}
+                countryCodesOptions={countryCodesOptions}
+                companyOptions={companyOptions}
+                objectiveOptions={objectiveOptions}
+                state={this.state}
+            />
         );
     }
 }
--- appointment-booker/src/components/appointment/appointment-view.js
+++ appointment-booker/src/components/appointment/appointment-view.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 import Select from 'react-select'
 import CreatableSelect from 'react-select/creatable';
 
-import CountryCodes from './country-codes'
 import backArrow from '../../assets/back-arrow.png'
 import infoIcon from '../../assets/info-icon-button.png'
 
@@ -16,24 +15,9 @@ function Appointment(props) {
         width: window.innerWidth < 768 ? '90%' : '40%',
     }
 
-    const options = [
-        { value: 'country1', label: 'Country1' },
-        { value: 'country2', label: 'Country2' },
-        { value: 'country3', label: 'Country3' }
-    ]
+    const state = props.state
 
-    const objectiveOptions = [
-        { value: 'complaint', label: 'Complaint' },
-        { value: 'suggestion', label: 'Suggestion' }
-    ]
-
-    const countryCodesOptions = CountryCodes.map(code => {
-        return (
-            <option key={code} value={code.dial_code}>
-                {`${code.code} ${code.dial_code}`} 
-            </option>
-        );
-    });
+    const unvalidStyle = '2px solid rgb(214, 68, 68)'
 
     return (
         <>
@@ -43,31 +27,73 @@ function Appointment(props) {
             <Form onSubmit={props.requestAppointment} className='book-appointment-form' style={style}>
                 <Form.Row>
                     <Form.Group as={Col}>
-                        <Form.Control type="text" name="firstName" placeholder="First Name" onChange={props.handleChange} />
+                        <Form.Control 
+                            type="text" 
+                            value={state.firstName} 
+                            name="firstName" 
+                            placeholder="First Name" 
+                            onChange={props.handleChange}
+                            style={{ borderBottom:state.validFirstName === false ? unvalidStyle : '' }}
+                        />
                     </Form.Group>
                     <Form.Group as={Col}>
-                        <Form.Control type="text" name="lastName" placeholder="Last Name" onChange={props.handleChange} />
+                        <Form.Control 
+                            type="text" 
+                            value={state.lastName} 
+                            name="lastName" 
+                            placeholder="Last Name" 
+                            onChange={props.handleChange} 
+                            style={{ borderBottom:state.validLastName === false ? unvalidStyle : '' }}
+                        />
                     </Form.Group>
                 </Form.Row>
 
                 <Form.Group>
-                    <Form.Control type="email" name="email" placeholder="Work Email Address" onChange={props.handleChange} />
+                    <Form.Control 
+                        type="email" 
+                        value={state.email} 
+                        name="email" 
+                        placeholder="Work Email Address" 
+                        onChange={props.handleChange} 
+                        style={{ borderBottom:state.validEmail === false ? unvalidStyle : '' }}
+                        />
                 </Form.Group>
 
                 <Form.Row>
                     <Form.Group as={Col} xs="auto">
-                        <Form.Control as="select" className="mr-sm-2" name="countryCode" custom onChange={props.handleChange}>
-                            { countryCodesOptions }
+                        <Form.Control 
+                            value={state.countryCode} 
+                            as="select" 
+                            className="mr-sm-2" 
+                            name="countryCode" 
+                            custom onChange={props.handleChange}
+                            style={{ borderBottom:state.validCountryCode === false ? unvalidStyle : '' }}
+                        >
+                            { props.countryCodesOptions }
                         </Form.Control>
                     </Form.Group>
                     <Form.Group as={Col}>
-                        <Form.Control type="number" name="phoneNumber" placeholder="Phone Number" onChange={props.handleChange} />
+                        <Form.Control 
+                            type="number" 
+                            value={state.phoneNumber} 
+                            name="phoneNumber" 
+                            placeholder="Phone Number" 
+                            onChange={props.handleChange} 
+                            style={{ borderBottom:state.validPhoneNumber === false ? unvalidStyle : '' }}
+                        />
                     </Form.Group>
                 </Form.Row>
                 
                 <Form.Row>
-                    <Form.Group as={Col}>
-                        <Select placeholder="Operation Countries" name="operationCountires" options={options} isMulti />
+                    <Form.Group as={Col} style={{ borderBottom: state.validOperationCountries === false ? unvalidStyle : '' }}>
+                        <Select
+                            onMenuOpen={() => props.setOpenedMenu('operationCountries')}
+                            placeholder="Operation Countries" 
+                            name="operationCountries" 
+                            onChange={props.handleChange} 
+                            options={props.countryOptions} 
+                            isMulti
+                        />
                     </Form.Group>
                     <Form.Group as={Col} xs={1} className="mt-1 mr-2">
                         <span>
@@ -77,11 +103,13 @@ function Appointment(props) {
                 </Form.Row>
 
                 <Form.Row>
-                    <Form.Group as={Col}>
+                    <Form.Group as={Col} style={{ borderBottom:state.validCompanyName === false ? unvalidStyle : '' }}>
                         <CreatableSelect 
                             placeholder="Company Name" 
-                            name="companyName" 
-                            options={options}
+                            name="companyName"
+                            onMenuOpen={() => props.setOpenedMenu('companyName')}
+                            options={props.companyOptions}
+                            onChange={props.handleChange}
                         />
                     </Form.Group>
                     <Form.Group as={Col} xs={1} className="mt-1 mr-2">
@@ -92,17 +120,27 @@ function Appointment(props) {
                 </Form.Row>
 
 
-                <Form.Group>
-                    <CreatableSelect 
+                <Form.Group style={{ borderBottom:state.validObjective === false ? unvalidStyle : '' }}>
+                    <CreatableSelect
+                        onMenuOpen={() => props.setOpenedMenu('objective')}
                         placeholder="Objective" 
-                        name="companyName" 
-                        options={objectiveOptions}
+                        name="objective" 
+                        options={props.objectiveOptions}
+                        onChange={props.handleChange}
                         isMulti
                     />
                 </Form.Group>
 
                 <Form.Group>
-                    <Form.Control as="textarea" name="description" placeholder="More Details/Description" rows={3} onChange={props.handleChange} />
+                    <Form.Control 
+                        as="textarea" 
+                        value={state.description} 
+                        name="description" 
+                        placeholder="More Details/Description" 
+                        rows={3} 
+                        onChange={props.handleChange} 
+                        style={{ borderBottom:state.validDescription === false ? unvalidStyle : '' }}
+                    />
                 </Form.Group>
 
                 <Form.Group>
