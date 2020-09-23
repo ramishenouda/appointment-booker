af30f4315a0fd046a85dc9ab73f8b7c77ebb623a
ramishenouda <ramishenouda@outlook.com>
2020-09-24 00:58:35 +0200

    more responsive for small devices

--- appointment-booker/src/components/home/home-view.js
+++ appointment-booker/src/components/home/home-view.js
@@ -7,6 +7,39 @@ import PharmacyLogo from '../../assets/Pharmacylogo.mp4'
 import './home-style.css'
 
 function Home(props) {
+    if (window.innerWidth < 768) {
+        return (
+            <div className="sm-view">
+                <div className="iframe-container-sm">
+                    {
+                        /* <iframe src="https://www.youtube.com
+                        /embed/zmZ7Pfjymr0?vq=hd1080&playlist=zmZ7Pfjymr0&mute=1&autoplay
+                        =1&loop=1&modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&the
+                        me=light&fs=0&color=white&controls=0&disablekb=1" width="560" heigh
+                        t="315" frameborder="0"></iframe>​ */
+                    }
+                    <video width="320" height="240" loop="on" muted="on" autoPlay="on">
+                        <source src={PharmacyLogo} type="video/mp4" />
+                    </video>
+                </div>
+                <div>
+                    <center>
+                        <div className="home-text-sm">
+                            You could request an appointment with our team.
+                        </div>
+                        <div className="button-wrapper">
+                            <Link to='/appointment'>
+                                    <Button className="button">
+                                        Request an appointment
+                                    </Button>
+                            </Link>
+                        </div>
+                    </center>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <>
             <div className="home-contents">
