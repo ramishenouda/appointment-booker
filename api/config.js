de83e6790253298f595456611ab80d90764224c1
ramishenouda <ramishenouda@outlook.com>
2020-09-24 09:50:56 +0200

    changed mail html

--- api/config.js
+++ api/config.js
@@ -2,6 +2,8 @@ const config = {
     dbName: 'appointments',
     dbPassword: '522762413791',
     PORT: 5000,
+    nodeMailerUser: 'bookapp24@gmail.com',
+    nodeMailerPassword: 522762413791
 }
 
 export default config
--- api/controllers/appointments.js
+++ api/controllers/appointments.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import nodemailer from 'nodemailer'
 
+import config from '../config.js'
+
 import appointment from '../models/appointment.js'
 import Appointment from '../models/appointment.js' 
 
@@ -9,8 +11,8 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     secure: false, // use SSL
     auth: {
-        user: 'bookapp24@gmail.com',
-        pass: 522762413791
+        user: config.nodeMailerUser,
+        pass: config.nodeMailerPassword
     },
     logger: true
 });
@@ -63,8 +65,7 @@ export const makeAppointment = (req, res) => {
                 to: requestBody.email,
                 subject: 'We got your request',
                 html: (`
-                    <p>We got your information</p>
-                    <p>we will back to you soon</p>
+                    <p>We got your information. We will back to you soon</p>
                     <p>FirstName: ${requestBody.firstName}</p>
                     <p>LastName: ${requestBody.lastName}</p>
                     <p>Email: ${requestBody.email}</p>
@@ -86,6 +87,19 @@ export const makeAppointment = (req, res) => {
             });
 
             mailOptions.to = 'bookapp24@gmail.com';
+            mailOptions.html = `                    
+                <p> New Request </p>
+                <p>FirstName: ${requestBody.firstName}</p>
+                <p>LastName: ${requestBody.lastName}</p>
+                <p>Email: ${requestBody.email}</p>
+                <p>PhoneNumber: ${requestBody.countryCode} ${requestBody.phoneNumber}</p>
+                <p>Operation countries: ${requestBody.operationCountries}</p>
+                <p>Company name: ${requestBody.companyName}</p>
+                <p>Objective: ${requestBody.objective}</p>
+                <p>Description: ${requestBody.description}</p>
+                <p>Request date: ${new Date()}</p>
+            `
+
             transporter.sendMail(mailOptions, function(error, info){
                 if (error) {
                     console.log(error);
